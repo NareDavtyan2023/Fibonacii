64938d3423fdbb8ad44c92f65c7cb1919347bf6b
Admin <Admin@DESKTOP-A06UMIO>
2023-06-12 21:38:34 +0400

    Add project files.

---
+++ Fibonacii/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Channels;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write(Fib(10));
+        }
+
+            static int Fib(int x)
+            {
+                if (x == 0) return 0;
+
+                int prev = 0;
+                int next = 1;
+                for (int i = 1; i < x; i++)
+                {
+                    int sum = prev + next;
+                    prev = next;
+                    next = sum;
+                }
+                return next;
+            
+            }
+        
+        
+    }
+}
